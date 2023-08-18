e7999470e4c8706d5933d470e76c209da8d7a443
Misbahudin <misbahudin.id@gmail.com>
2023-08-18 10:07:27 +0700

    remove: login with social media

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,10 +2,6 @@
 
     @section("title", "Login Page")
 
-    @section("css")
-    <link rel="stylesheet" href="{{asset("assets/library/bootstrap-social/bootstrap-social.css")}}">
-    @endsection
-
     @section("content")
     <div class="row">
         <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
@@ -54,26 +50,7 @@
                     </button>
                 </div>
                 </form>
-                <div class="text-center mt-4 mb-3">
-                <div class="text-job text-muted">Login With Social</div>
-                </div>
-                <div class="row sm-gutters">
-                <div class="col-6">
-                    <a class="btn btn-block btn-social btn-facebook">
-                    <span class="fab fa-facebook"></span> Facebook
-                    </a>
-                </div>
-                <div class="col-6">
-                    <a class="btn btn-block btn-social btn-twitter">
-                    <span class="fab fa-twitter"></span> Twitter
-                    </a>
-                </div>
-                </div>
-
             </div>
-            </div>
-            <div class="my-4 text-muted text-center">
-            Don't have an account? <a href="/register">Create One</a>
             </div>
         </div>
         </div>
