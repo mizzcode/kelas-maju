c6b07d07a1ca74e37c6c4fefaa38e9d54b34af37
Misbahudin <misbahudin.id@gmail.com>
2023-08-20 18:38:43 +0700

    add: relasi one to many to model mapel and teacher

--- database/migrations/2023_08_19_200933_create_teachers_table.php
+++ database/migrations/2023_08_19_200933_create_teachers_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('teachers', function (Blueprint $table) {
             $table->uuid("id")->primary();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->enum("role", ["admin", "teacher"]);
+            $table->string("name");
+            $table->integer("nip");
+            $table->string("education");
+            $table->enum("gender", ["Pria", "Wanita"]);
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('teachers');
     }
 };
