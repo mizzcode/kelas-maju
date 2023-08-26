f83c651e6bbaaaefa8b542ce8454c163b6f6de32
Misbahudin <misbahudin.id@gmail.com>
2023-08-26 18:27:18 +0700

    fix: action route

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
             <div class="card-header"><h4>Login</h4></div>
 
             <div class="card-body">
-                <form method="POST" action="{{route("login")}}" class="needs-validation" novalidate="">
+                <form method="POST" action="{{route("postLogin")}}" class="needs-validation">
                 @csrf
                 <div class="form-group">
                     <label for="email">Email</label>
