16bb36cc4be2308402b5a26d27eba704f6212b12
Misbahudin <misbahudin.id@gmail.com>
2024-02-22 22:18:32 +0700

    fix: sweetalert not showing when email or password user is wrong

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Error;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -14,20 +16,22 @@ class LoginController extends Controller
     }
 
     public function login(Request $request) {
+        try {
         // melakukan validasi sebelum percobaan masuk
         $credentials = $this->validate($request, [
             "email" => "required|email:dns",
             "password" => "required|max:16"
         ]);
-        // jika user tervalidasi dengan benar maka redirect ke dashboard
-        if (Auth::attempt($credentials)) {
-            // membuat sesi baru
-            $request->session()->regenerate();
-
-            return redirect()->route("dashboard")->with("login", "Selamat Datang!");
-        } else {
-            // tiba-tiba tidak berfungsi
+        // jika user tidak tervalidasi maka throw error
+        if (!Auth::attempt($credentials)) throw new Error();
+        
+        // membuat sesi baru
+        $request->session()->regenerate();
+        // redirect ke route dashboard
+        return redirect()->route("dashboard")->with("login", "Selamat Datang!");
+        } catch(ValidationException) {
             return back()->with("errorLogin", "Email atau Password salah");
         }
+
     }
 }
