8aab6bb4a1771c728cc915ec606c105f76b08f2d
Misbahudin <misbahudin.id@gmail.com>
2024-02-23 22:23:32 +0700

    style: change margin top when display > 768px

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 
     @section("content")
     <div class="row">
-        <div class="mt-5 col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+        <div class="mt-md-4 col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand"></div>
 
             <div class="card card-primary">
