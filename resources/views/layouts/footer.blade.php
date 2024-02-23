59a68563e17f56d49c3d05dc323501531953bc4c
Misbahudin <misbahudin.id@gmail.com>
2024-02-23 22:23:45 +0700

    chore: change name

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
   <div class="footer text-center">
     Copyright &copy; 2024. Made with <i class="fa-solid fa-heart" style="color: #ff0000;"></i> by <a
-      href="https://github.com/mizzcode/" target="_blank">Misbahudin</a> & <a href="https://github.com/galihap76/"
-      target="_blank">Galih Anggoro Prasetya</a>
+      href="https://github.com/mizzcode/" target="_blank">Misbah</a> & <a href="https://github.com/galihap76/"
+      target="_blank">Galih</a>
   </div>
 </footer>
