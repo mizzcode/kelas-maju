1f66dab156bc0443bc25fffb6430df23039256d3
Misbahudin <misbahudin.id@gmail.com>
2024-02-23 22:11:59 +0700

    style: add margin top

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,9 +4,8 @@
 
     @section("content")
     <div class="row">
-        <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-            <div class="login-brand">
-            </div>
+        <div class="mt-5 col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="login-brand"></div>
 
             <div class="card card-primary">
             <div class="card-header"><h4>Login</h4></div>
