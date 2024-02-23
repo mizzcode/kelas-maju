c697b02e62844cd230ebeb31cab310f6235a2aa4
Misbahudin <misbahudin.id@gmail.com>
2024-02-23 22:08:57 +0700

    chore: change year to 2024

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
   <div class="footer text-center">
-    &copy; 2023 Crafted With <i class="fa-solid fa-heart" style="color: #ff0000;"></i> By <a
+    Copyright &copy; 2024. Made with <i class="fa-solid fa-heart" style="color: #ff0000;"></i> by <a
       href="https://github.com/mizzcode/" target="_blank">Misbahudin</a> & <a href="https://github.com/galihap76/"
       target="_blank">Galih Anggoro Prasetya</a>
   </div>
