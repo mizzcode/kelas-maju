b47ea6ce9bf93975b8b235b5eb77d39efbde9764
Misbahudin <misbahudin.id@gmail.com>
2024-02-24 17:21:06 +0700

    refactor

--- app/Http/Controllers/Admin/MapelController.php
+++ app/Http/Controllers/Admin/MapelController.php
@@ -7,13 +7,14 @@ use Illuminate\Support\Facades\Validator;
 
 use App\Models\Mapel;
 use App\Models\Teacher;
-
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class MapelController extends Controller
 {
 
-    private function validateForm(Request $request, array $rules)
+    private function useValidator(Request $request, array $rules)
     {
         $validator = Validator::make($request->all(), $rules);
         return $validator;
@@ -21,87 +22,89 @@ class MapelController extends Controller
 
     public function index()
     {
-        $mapel = Mapel::query()->latest()->paginate(5);
-        $teacherIds = Teacher::select('id', 'name')->get();
+        $mapels = Mapel::query()->latest()->paginate(5);
+        $teachers = Teacher::query()->latest()->paginate(5);
 
         $title = "Hapus Mata Pelajaran!";
         $text = "Apakah Anda yakin untuk menghapus?";
         confirmDelete($title, $text);
 
         return view("admin.mapel.index", [
-            "mapel" => $mapel,
-            "teachersIds" => $teacherIds
+            "mapels" => $mapels,
+            "teachers" => $teachers
         ]);
     }
 
     public function store(Request $request)
     {
-        $validator = $this->validateForm($request, [
-            "name" => "required"
-        ]);
-
-        // jika gagal validasi sesuai rules
-        if ($validator->fails()) {
-            return redirect()->back()
-                ->withErrors($validator)
-                ->with("errorCreateMapel", "Gagal menambah mata pelajaran baru.");
-        } else {
-
-            // Lakukan insert
-            $insertMapel = Mapel::create([
-                'name' => $request->name,
-                'teacher_id' => $request->teacher_id
+        try {
+            $validator = $this->useValidator($request, [
+                "name" => "required",
+                "teacher_id" => "required"
             ]);
-
-            if ($insertMapel) {
-
-                return redirect()->route("mapel.index")->with("successCreateMapel", "Berhasil menambahkan mata pelajaran baru.");
+    
+            // jika gagal validasi sesuai rules
+            if ($validator->fails()) {
+                return redirect()->back()
+                ->withErrors($validator)
+                ->withInput()
+                ->with("errorCreateMapel", "Gagal menambah mata pelajaran baru.");
             }
+            // menerima input yang sudah tervalidasi
+            $validated = $validator->validated();
+    
+            // Lakukan insert
+            Mapel::query()->create($validated);
+
+            return redirect()->route("mapel.index")->with("successCreateMapel", "Berhasil menambahkan mata pelajaran baru.");
+        } catch (QueryException $e) {
+            Log::info($e);
+            dd($e->getMessage());
+            return redirect()->back()->with("errorCreateMapel", "Gagal menambah mata pelajaran baru.");
         }
     }
 
     public function update(Request $request)
     {
-
-        $validator = $this->validateForm($request, [
-            "name" => "required"
-        ]);
-
-        // jika gagal validasi sesuai rules
-        if ($validator->fails()) {
-            return redirect()->back()
-                ->with("requiredFieldsUpdate", "Anda harus mengisi bidang yang tersedia.");
-        } else {
-
-            // Lakukan update berdasarkan ID mapel pada modal update
+        try {
             $mapel = Mapel::query()->findOrFail($request->mapel_id);
 
-            $mapel->name = $request->name;
-            $mapel->teacher_id = $request->teacher_id;
-
-            // Cek jika berhasil di update
-            if ($mapel->save()) {
-
-                // Kirim pesan
-                return redirect()->route("mapel.index")->with("successUpdateMapel", "Data mata pelajaran berhasil di update.");
-
-                // Kalo gagal
-            } else {
-                back()->with("errorUpdateMapel", "Gagal memperbarui data mata pelajaran.");
+            $validator = $this->useValidator($request, [
+                "name" => "required",
+                "teacher_id" => "required"
+            ]);
+    
+            // jika gagal validasi sesuai rules
+            if ($validator->fails()) {
+                return redirect()->back()
+                ->withErrors($validator)
+                ->withInput()
+                ->with("errorUpdateMapel", "Gagal menambah mata pelajaran baru.");
             }
+            // menerima input yang sudah tervalidasi
+            $validated = $validator->validated();
+    
+            // Lakukan insert
+            Mapel::query()->where('id', '=', $mapel->id)->update($validated);
+
+            return redirect()->route("mapel.index")->with("successUpdateMapel", "Berhasil menambahkan mata pelajaran baru.");
+        } catch (QueryException $e) {
+            Log::info($e);
+            dd($e->getMessage());
+            return redirect()->back()->with("errorUpdateMapel", "Gagal menambah mata pelajaran baru.");
         }
     }
 
     public function destroy(string $id)
     {
-
-        $mapel = Mapel::query()->findOrFail($id);
-
-        if ($mapel->delete()) {
+        try {
+            Mapel::query()->where('id', '=', $id)->delete();
 
             return redirect()->route("mapel.index")->with("successDeleteMapel", "Data mata pelajaran berhasil di delete.");
-        } else {
+        } catch (QueryException $e) {
+            Log::info($e);
+            dd($e->getMessage());
             return back()->with("errorDeleteMapel", "Gagal menghapus data mata pelajaran.");
-        }
+        } 
     }
 }
