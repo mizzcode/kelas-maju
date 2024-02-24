f8efc80192413c662f06e4d7f23f2a679032453a
Misbahudin <misbahudin.id@gmail.com>
2024-02-24 17:22:01 +0700

    fix: default value uuid

--- app/Models/Mapel.php
+++ app/Models/Mapel.php
@@ -2,13 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Mapel extends Model
 {
+    use HasUuids, HasFactory;
+    
     protected $table = "mapels";
+    
+    public $incrementing = false;
+    
+    protected $keyType = "string";
 
     protected $fillable = [
         "name",
