2638672f4e1a9cc7c3e8a2f82329b05578fc7ce9
Misbahudin <misbahudin.id@gmail.com>
2024-03-13 18:04:59 +0700

    refactor

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,9 +7,6 @@
       <a href="#">KM</a>
     </div>
     <ul class="sidebar-menu">
-      <li class="menu-header">MENU</li>
-      <li class="nav-item {{Request::is('admin') ? 'active' : ''}}">
-
         {{-- Solusi fix prettier untuk remove spasi pada route --}}
         @php
         $dashboard = "dashboard";
@@ -18,20 +15,22 @@
         $teacherIndex = "teacher.index";
         $mapelIndex = "mapel.index";
         @endphp
-
+        
+      <li class="menu-header">MENU</li>
+      <li class="nav-item {{Request::is('admin') ? 'active' : ''}}">
         <a href="{{route($dashboard)}}"><i class="fas fa-fire"></i><span>Dashboard</span></a>
       </li>
       <li class="nav-item {{Request::is('admin/user') ? 'active' : ''}}">
-        <a href="{{route($userIndex)}}"><i class="fa-solid fa-user"></i><span>Pengguna</span></a>
+        <a href="{{route($userIndex)}}"><i class="fas fa-user"></i><span>Pengguna</span></a>
       </li>
       <li class="nav-item {{Request::is('admin/student') ? 'active' : ''}}">
-        <a href="{{route($studentIndex)}}"><i class="fa-solid fa-graduation-cap"></i><span>Siswa</span></a>
+        <a href="{{route($studentIndex)}}"><i class="fas fa-graduation-cap"></i><span>Siswa</span></a>
       </li>
       <li class="nav-item {{Request::is('admin/teacher') ? 'active' : ''}}">
-        <a href="{{route($teacherIndex)}}"><i class="fa-solid fa-graduation-cap"></i><span>Guru</span></a>
+        <a href="{{route($teacherIndex)}}"><i class="fas fa-graduation-cap"></i><span>Guru</span></a>
       </li>
       <li class="nav-item {{Request::is('admin/mapel') ? 'active' : ''}}">
-        <a href="{{route($mapelIndex)}}"><i class="fa-solid fa-graduation-cap"></i><span>Mapel</span></a>
+        <a href="{{route($mapelIndex)}}"><i class="fas fa-calendar-alt"></i><span>Mapel</span></a>
       </li>
     </ul>
   </aside>
